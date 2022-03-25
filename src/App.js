b40774e4b27299f64ad6dfd0364c436660ca5c7f
Emmanuel Utomi <emmanuelutomi@gmail.com>
2022-03-25 04:37:28 -0500

    message sign

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   const [address, setAddress] = useState(0);
   const [bal, setBal] = useState('N/A');
+  const [message, setMessage] = useState('');
+  const [provider, setProvider] = useState();
+  const [signature, setSignature] = useState();
 
 
   // Helper Functions
@@ -32,6 +35,7 @@ function App() {
           let etherString = ethers.utils.formatEther(balance) + " ETH";
 
           setBal(etherString);
+          setProvider(provider)
         });
       } catch (error) {
         alert('Error connecting...');
@@ -65,16 +69,58 @@ function App() {
 
   }
 
+  async function changeMessage(e) {
+    setMessage(e.target.value)
+  }
+
+  async function signMessage() {
+    if (message == '') {
+      alert('Message is empty');
+    }
+    else {
+
+      // Sign the string message
+      let signer = await provider.getSigner()
+
+      // For Solidity, we need the expanded-format of a signature
+      // let sig = ethers.utils.splitSignature(flatSig);
+
+      let signature = await signer.signMessage(message);
+
+      setSignature(signature)
+    }
+  }
+
+  const disabled = address == 0 || address == -1
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Wallet: {address == -1 ? 'Error' : (address ? address : 'N/A')}</h3>
-        <h3>Balance: {address ? bal : 'N/A'}</h3>
+        <h3>Wallet Address: {disabled ? 'N/A' : address}</h3>
+        <h3>Current Balance: {disabled ? 'N/A' : bal}</h3>
         <button disabled={address}
           onClick={connectWallet}> {address == -1 ? 'Error' : address == 0 ? 'Connect Wallet' : 'Connected'}</button>
         <br />
-        <button disabled={address == 0}
+        <button disabled={disabled}
           onClick={disconnectWallet}>Disconnect</button>
+
+
+        <br />
+        <br />
+        <div style={{ visibility: disabled ? 'hidden' : 'visible' }}>
+          <input value={message} onChange={changeMessage} disabled={signature} disabled={disabled} placeholder='message'></input>
+          <br />
+          <br />
+          <h3 >Message: {message}</h3>
+          <button onClick={signMessage} disabled={disabled || signature}>Sign + Deploy Message</button>
+
+          <div style={{ visibility: signature ? 'visible' : 'hidden' }}>
+          <h3>Signature:</h3> 
+          <code>{signature}</code>
+          <h2><a target="_blank" href="https://etherscan.io/verifiedSignatures">Verify Signature on Etherscan</a></h2>
+          You will need:<br/>
+          <em>Address + Message + Signature</em>
+          </div>
+        </div>
       </header>
     </div>
   );
